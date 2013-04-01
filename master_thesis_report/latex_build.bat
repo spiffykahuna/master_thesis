db513cbfae2d9515b1f473b46cf0cb742f7b46d6
Denis Konstantinov <deko1988@hot.ee>
2013-04-01 18:00:33 +0300

    Launched environment on windows Vista. Now it seems too work

---
+++ master_thesis_report/latex_build.bat
@@ -0,0 +1,8 @@
+@echo off
+
+:C:
+:chdir C:\cygwin\bin
+
+set PATH=C:\cygwin\bin;%PATH%
+
+c:\cygwin\bin\bash.exe /cygdrive/c/Users/Kasutaja/Desktop/master_thesis/master_thesis/master_thesis_report/latex_build.sh %1
--- master_thesis_report/latex_build.sh
+++ master_thesis_report/latex_build.sh
@@ -1,9 +1,9 @@
- #!/bin/sh
+#!/bin/sh
 echo 'We are in:'
 pwd
 
 rm *.bbl
-rm $1.pdf
+rm "$1".pdf
 rm *.aux
 rm *.glo
 rm *.glg
@@ -16,12 +16,13 @@ rm *.log
 #./texcount_2_1.pl -1 Introduction.tex Aim.tex Contribution.tex BodyOfKnowledge.tex RESTful_POG.tex > wordcount.tex
 #cat wordcount.tex |sed 's/\([0-9]*\)+.*/\1/' > wordcount2.tex
 cp ../tex/*.tex ./
-pdflatex -interaction=scrollmode --src-specials $1.tex
-bibtex $1
-pdflatex -interaction=scrollmode --src-specials $1.tex
+pdflatex -interaction=scrollmode --src-specials "$1".tex
+bibtex "$1"
+pdflatex -interaction=scrollmode --src-specials "$1".tex
 #makeindex -s $1.ist -t $1.glg -o $1.gls $1.glo
-makeglossaries $1
-pdflatex -interaction=scrollmode --src-specials $1.tex
+makeglossaries "$1"
+pdflatex -interaction=scrollmode --src-specials "$1".tex
+cp "$1".pdf ../out/
 
-cp $1.pdf ../out/
-
+echo 'OUTPUT_FOLDER:'
+ls ../out/
