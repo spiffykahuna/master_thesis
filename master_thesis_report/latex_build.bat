26dbfade1bc1f72477fcbe8cbe4d7b0a3434c17c
Denis Konstantinov <deko1988@hot.ee>
2013-08-13 03:08:27 +0300

    Hardware and freertos desctription was developed

--- master_thesis_report/latex_build.bat
+++ master_thesis_report/latex_build.bat
@@ -3,6 +3,6 @@
 :C:
 :chdir C:\cygwin\bin
 
-set PATH=C:\cygwin\bin;%PATH%
+set PATH=C:\cygwin\bin;C:\texlive\2013\bin\win32;%PATH%
 
 c:\cygwin\bin\bash.exe /cygdrive/c/Users/Kasutaja/Desktop/master_thesis/master_thesis/master_thesis_report/latex_build.sh %1
